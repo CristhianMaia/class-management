f5a50e0376e55210f8da643cf2898e680051ff4d
Rafael Zottesso <zottesso@hotmail.com>
2018-05-23 12:13:42 -0300

    Configuração de classes que vão aparecer no admin do Django.

--- tcc/classmanagement/admin.py
+++ tcc/classmanagement/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Colegio)
+admin.site.register(Professor)
+admin.site.register(Turma)
+admin.site.register(Materia)
+admin.site.register(Avisos)
+admin.site.register(Atendimento)
