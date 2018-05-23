b389c73c3de2013ec7eadae100ddaaadfc8debdf
Rafael Zottesso <zottesso@hotmail.com>
2018-05-23 12:04:53 -0300

    Ajustes nas classes do Models.

--- tcc/classmanagement/models.py
+++ tcc/classmanagement/models.py
@@ -3,6 +3,7 @@ from django.db import models
 #updated_at = models.DateTimeField(auto_now=True)
 #horarios: CharField:length=5
 from django.contrib.auth.models import User
+
 class Colegio(models.Model):
     nome                    = models.CharField(max_length=240, blank= False, null=False, help_text='Obrigatório.')
     data_cadastro           = models.DateField(auto_now_add=True)
@@ -13,10 +14,10 @@ class Professor(models.Model):
 
 class Turma(models.Model):
     nome                    = models.CharField(max_length=120, blank=False, null=False, help_text='Obrigatório.')
-    alunos                  = models.ManyToManyField(User, on_delete=models.CASCADE, null=False)
+    alunos                  = models.ManyToManyField(User, help_text='Obrigatório', related_name='alunos_matriculados')
     data_cadastro           = models.DateField(auto_now_add=True)
-    representante           = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    colegio                 = models.ForeignKey(Colegio, null=False, blank=False, help_text='Obrigatório.')
+    representante           = models.ForeignKey(User, on_delete=models.PROTECT, blank=False, null=False)
+    colegio                 = models.ForeignKey(Colegio, on_delete=models.PROTECT, blank=False, null=False, help_text='Obrigatório.')
 
 
 class Materia(models.Model):
@@ -29,13 +30,13 @@ class Materia(models.Model):
         ('Sab', 'Sabado'),
         ('Dom', 'Domingo'),
     )
-    nome                    = models.CharField(max_length=120, help_text='Obrigatório.')
-    local                   = models.CharField(max_length=120, help_text='Obrigatório.')
-    dia                     = models.CharField(max_length=3, choices=DIA_CHOICES, help_text='Obrigatório.')
+    nome                    = models.CharField(max_length=120, blank=False, null=False, help_text='Obrigatório.')
+    local                   = models.CharField(max_length=120, blank=False, null=False, help_text='Obrigatório.')
+    dia                     = models.CharField(max_length=3, blank=False, null=False, choices=DIA_CHOICES, help_text='Obrigatório.')
     horario_inicio          = models.CharField(max_length=5, blank=False, null=False)
     horario_fim             = models.CharField(max_length=5, blank=False, null=False)
-    turma                   = models.ForeignKey(Turma, null=False, blank=False)
-    professor               = models.ForeignKey(Professor, null=False, blank=False)
+    turma                   = models.ForeignKey(Turma, on_delete=models.PROTECT, null=False, blank=False)
+    professor               = models.ForeignKey(Professor, on_delete=models.PROTECT, null=False, blank=False)
 
 class Avisos(models.Model):
     AVISO_CHOICES   = (
@@ -45,12 +46,12 @@ class Avisos(models.Model):
         ('E', 'Prova'), #Exam
     )
     data_final              = models.DateField(auto_now=False, auto_now_add=False, blank=False, null=False)
-    tipo_aviso              = models.CharField(max_length=1, choices=AVISO_CHOICES, help_text='Obrigatório.')
+    tipo_aviso              = models.CharField(max_length=1, choices=AVISO_CHOICES, null=False, blank=False, help_text='Obrigatório.')
     comentarios             = models.TextField(null=True, blank=True)
     ultima_modificacao      = models.DateField(auto_now=True)
     data_post               = models.DateField(auto_now_add=True)
-    turma                   = models.ForeignKey(Turma, null=False, blank=False)
-    materia                 = models.ForeignKey(Materia, null=False, blank=False)
+    turma                   = models.ForeignKey(Turma, on_delete=models.PROTECT, null=False, blank=False)
+    materia                 = models.ForeignKey(Materia, on_delete=models.PROTECT, null=False, blank=False)
 
 class Atendimento(models.Model):
     DIA_CHOICES   = (
@@ -62,8 +63,8 @@ class Atendimento(models.Model):
         ('Sab', 'Sabado'),
         ('Dom', 'Domingo'),
     )
-    turma                   = models.ForeignKey(Turma, null=False)
-    professor               = models.ForeignKey(Professor, null=False)
+    turma                   = models.ForeignKey(Turma, on_delete=models.PROTECT, blank=False, null=False, help_text='Obrigatório.')
+    professor               = models.ForeignKey(Professor, on_delete=models.PROTECT, blank=False, null=False, help_text='Obrigatório.')
     dia                     = models.CharField(max_length=3, choices=DIA_CHOICES, blank=False, null=False, help_text='Obrigatório.')
-    horario_inicio          = models.CharField(max_length=5, blank=False, null=False)
-    horario_fim             = models.CharField(max_length=5, blank=False, null=False)
+    horario_inicio          = models.CharField(max_length=5, blank=False, null=False, help_text='Obrigatório.')
+    horario_fim             = models.CharField(max_length=5, blank=False, null=False, help_text='Obrigatório.')
