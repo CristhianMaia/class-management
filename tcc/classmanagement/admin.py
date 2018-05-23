b33ed69dd56607fdbc6bdf1421c80fc81be5669e
Rafael Zottesso <zottesso@hotmail.com>
2018-05-23 12:27:34 -0300

    Apenas adição de alguns comentários.

--- tcc/classmanagement/admin.py
+++ tcc/classmanagement/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Importa todas as classes do seu models
 from .models import *
 
+# Especifica que todas as classes abaixo vão aparecer no menu admin do Django
 admin.site.register(Colegio)
 admin.site.register(Professor)
 admin.site.register(Turma)
